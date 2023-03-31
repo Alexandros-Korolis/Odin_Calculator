c839355b0a1b88bce91c3d8702f5fd306e3b9500
alexandros <alex.korolis@gmail.com>
2023-03-31 18:49:23 +0200

    added functions

---
+++ javascript.js
@@ -0,0 +1,29 @@
+const add = function(num1,num2,op){
+    let num1 ;
+    let num2 ;
+    let op ;
+};
+
+const subtract = function(num1,num2,op){
+    let num1 ;
+    let num2 ;
+    let op ;
+};
+
+const multiply = function(num1,num2,op){
+    let num1 ;
+    let num2 ;
+    let op ;
+};
+
+const divide = function(num1,num2,op){
+    let num1 ;
+    let num2 ;
+    let op ;
+};
+
+const operate = function(num1,num2,op){
+    let num1 ;
+    let num2 ;
+    let op ;
+};
